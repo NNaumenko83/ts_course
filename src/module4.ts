acfab4763e2a817c5ce7e3c3f6c848a8cbdd0fed
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-07-03 23:19:45 +0300

    Add abstract classes

--- src/module4.ts
+++ src/module4.ts
@@ -13,22 +13,147 @@
 
 // b.showPropery();
 
-class House {
-  street: string;
+// class House {
+//   street: string;
 
-  constructor(n: string) {
-    this.street = n;
+//   constructor(n: string) {
+//     this.street = n;
+//   }
+
+//   showAdress(this: House): void {
+//     console.log("Adress: " + this.street);
+//   }
+// }
+
+// const house = new House("Middle-earth");
+
+// house.showAdress();
+
+// const copyHouse = { showAdress: house.showAdress, street: "Mykola street" };
+
+// copyHouse.showAdress();
+
+// Модифікатори доступа
+
+// class House {
+//   private tetants: string[] = [];
+
+//   constructor(protected street: string, public readonly type: string) {
+//     this.street = street;
+//   }
+
+//   public showAdress(this: House, add: string) {
+//     console.log("Adress " + this.street + add);
+//   }
+
+//   public addTenant(name: string) {
+//     this.tetants.push(name);
+//   }
+//   public showTenants() {
+//     console.log(this.tetants);
+//   }
+
+//   public showType() {
+//     console.log(this.type);
+//   }
+// }
+
+// const house = new House("Middle-earth", "wood");
+// console.log("house:", house);
+
+// house.showAdress("Hello");
+
+// house.addTenant("Max");
+// house.addTenant("Nikita");
+
+// house.shoqTenants();
+
+// house.showType();
+
+// class StoneHouse extends House {
+//   private chargeOfHouse: string;
+//   constructor(street: string, general: string) {
+//     super(street, "stone");
+
+//     this.chargeOfHouse = general;
+//   }
+
+//   public showTenants() {
+//     console.log("General:" + this.chargeOfHouse);
+//     super.showTenants();
+//   }
+
+//   public showAdress(): void {
+//     console.log("Adress " + this.street);
+//   }
+// }
+
+// const stoneHouse = new StoneHouse("stone-word", "Max");
+
+// stoneHouse.addTenant("Mykola");
+// stoneHouse.addTenant("Varui");
+
+// stoneHouse.showTenants();
+
+// Статичні методи і властивості
+
+// class UseStatic {
+//   private static count = 0;
+
+//   constructor() {
+//     UseStatic.count += 1;
+//   }
+
+//   public static isStatic() {
+//     console.log("Im static");
+//   }
+
+//   public showCount() {
+//     console.log(UseStatic.count);
+//   }
+// }
+
+// const obj1 = new UseStatic();
+// const obj2 = new UseStatic();
+// const obj3 = new UseStatic();
+
+// obj1.showCount();
+// obj2.showCount();
+// obj3.showCount();
+
+// UseStatic.isStatic();
+
+// ===== Abstract clases =====
+
+abstract class Plane {
+  protected pilotInCabin = false;
+
+  public sitInPlane() {
+    this.pilotInCabin = true;
   }
 
-  showAdress(this: House): void {
-    console.log("Adress: " + this.street);
+  public abstract startEngine(): string;
+}
+
+class Meize extends Plane {
+  public startEngine(): string {
+    {
+      return "ta-ta-ta";
+    }
   }
 }
 
-const house = new House("Middle-earth");
+class Boeing extends Plane {
+  public startEngine(): string {
+    return "Buuuuuuu";
+  }
+}
 
-house.showAdress();
+const maize = new Meize();
+const boeing = new Boeing();
 
-const copyHouse = { showAdress: house.showAdress, street: "Mykola street" };
+maize.sitInPlane();
+boeing.sitInPlane();
 
-copyHouse.showAdress();
+console.log(maize.startEngine());
+console.log(boeing.startEngine());

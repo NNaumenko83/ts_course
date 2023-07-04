090251ea52cd2ff822197338f3604ad3c9c7f633
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-07-04 20:26:40 +0300

    done task 4

--- src/app.ts
+++ src/app.ts
@@ -1,223 +1,223 @@
-// const button = document.querySelector("button");
-// const input1 = document.getElementById("num1")! as HTMLInputElement;
-// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// // const button = document.querySelector("button");
+// // const input1 = document.getElementById("num1")! as HTMLInputElement;
+// // const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-// function add(num1: number, num2: number) {
-//   return num1 + num2;
-// }
+// // function add(num1: number, num2: number) {
+// //   return num1 + num2;
+// // }
 
-// button?.addEventListener("click", function () {
-//   console.log(add(+input1.value, +input2.value));
-// });
+// // button?.addEventListener("click", function () {
+// //   console.log(add(+input1.value, +input2.value));
+// // });
 
-// let num: number;
+// // let num: number;
 
-// num = 10;
+// // num = 10;
 
-// let str: string;
+// // let str: string;
 
-// str = "12";
+// // str = "12";
 
-// let boo: boolean;
+// // let boo: boolean;
 
-// boo = true;
+// // boo = true;
 
-// let person = 'Max'
+// // let person = 'Max'
 
-// person='2'
+// // person='2'
 
-// let arr: string[] = [];
+// // let arr: string[] = [];
 
-// arr = ["str2", "srt"];
+// // arr = ["str2", "srt"];
 
-// let arrObj: { name: string }[];
+// // let arrObj: { name: string }[];
 
-// arrObj = [{ name: "Max" }, { name: "Alex" }];
+// // arrObj = [{ name: "Max" }, { name: "Alex" }];
 
-// let obj: { name: string };
+// // let obj: { name: string };
 
-// obj = { name: "Alex" };
+// // obj = { name: "Alex" };
 
-// obj.name;
+// // obj.name;
 
-// let db: { id: number; title: string; info: { date: Date; isNew: boolean } };
-// db = { id: 1, title: "new product", info: { date: new Date(), isNew: true } };
+// // let db: { id: number; title: string; info: { date: Date; isNew: boolean } };
+// // db = { id: 1, title: "new product", info: { date: new Date(), isNew: true } };
 
-// Тип ANY  - не проходить перевірок!!!
+// // Тип ANY  - не проходить перевірок!!!
+// // let some: any;
+
+// // some = 1;
+// // some = "String";
+
+// // Тип unknown  - не проходить перевірок!!!
+// // let some: unknown;
+
+// // some = 1;
+// // some = "String";
+
+// // let str: string;
+// // if (typeof some === "string") {
+// //   str = some;
+// // }
+
+// // let fixed: [string, number];
+// // fixed = ["Nick", 2];
+
+// // ENUM
+
+// // enum Toggle {
+// //   ENABLE,
+// //   DISABLE,
+// // }
+
+// // const service = { status: Toggle.ENABLE };
+
+// // if (service.status === Toggle.ENABLE) {
+// //   console.log("It is active");
+// // }
+
+// // UNION TYPE
+
+// // let union: number | string
+
+// // union = 10
+
+// // function combine(param1: string | number, param2: string | number) {
+// //   if (typeof param1 === "string" || typeof param2 === "string") {
+// //     return param1.toString() + param2.toString();
+// //   }
+
+// //   return param1 + param2;
+// // }
+
+// // console.log(combine("str1", "str2"));
+
+// // let active: "start" | "and";
+
+// // active = "start";
+
+// // const fruit: string[] = [];
+
+// // function workWithArr(arr: string[], value: string, action: "add" | "delete") {
+// //   if (action === "add") {
+// //     arr.push(value);
+// //   } else {
+// //     const index = arr.indexOf(value);
+// //     arr.splice(index, 1);
+// //   }
+// //   return arr;
+// // }
+
+// // workWithArr(fruit, "Banana", "add");
+// // workWithArr(fruit, "Melon", "add");
+// // workWithArr(fruit, "Pear", "add");
+// // workWithArr(fruit, "Bear", "add");
+
+// // console.log(fruit);
+
+// // function calc(
+// //   num1: number,
+// //   num2: number,
+// //   callback: (arg1: number, arg2: number) => number
+// // ) {
+// //   return callback(num1, num2);
+// // }
+// // function foo(num1: number, num2: number) {
+// //   return num1 + num2;
+// // }
+
+// // const result = calc(1, 3, foo);
+// // console.log("result:", result);
+
+// // ПРАКТИКА
+// // 1
+// let age: number;
+// // let name: string;
+// let toggle: boolean;
+// let empty: null;
+// let notInitialize: undefined;
+// let callback: (a: number) => number;
+// callback = (a) => {
+//   return 100 + a;
+// };
+
+// // 2
+// let anything: any;
+// anything = "Text";
+// anything = {};
+
+// // 3
 // let some: any;
-
-// some = 1;
-// some = "String";
-
-// Тип unknown  - не проходить перевірок!!!
-// let some: unknown;
-
-// some = 1;
-// some = "String";
+// some = "Text";
 
 // let str: string;
-// if (typeof some === "string") {
-//   str = some;
+// str = some;
+
+// // 4
+// let person: [string, number];
+// person = ["Max", 21];
+
+// // 5
+// enum Load {
+//   LOADING,
+//   READY,
 // }
 
-// let fixed: [string, number];
-// fixed = ["Nick", 2];
+// const page = { load: Load.READY };
 
-// ENUM
-
-// enum Toggle {
-//   ENABLE,
-//   DISABLE,
+// if (page.load === Load.LOADING) {
+//   console.log("Loading");
 // }
 
-// const service = { status: Toggle.ENABLE };
-
-// if (service.status === Toggle.ENABLE) {
-//   console.log("It is active");
+// if (page.load === Load.READY) {
+//   console.log("Ready");
 // }
 
-// UNION TYPE
+// // 6
 
-// let union: number | string
+// let strOrNumber: string | number;
 
-// union = 10
+// // 7
+// let strNew: "enable" | "Disable";
 
-// function combine(param1: string | number, param2: string | number) {
-//   if (typeof param1 === "string" || typeof param2 === "string") {
-//     return param1.toString() + param2.toString();
-//   }
-
-//   return param1 + param2;
+// // 8
+// function showMessage(message: string): void {
+//   console.log(message);
 // }
 
-// console.log(combine("str1", "str2"));
-
-// let active: "start" | "and";
-
-// active = "start";
-
-// const fruit: string[] = [];
-
-// function workWithArr(arr: string[], value: string, action: "add" | "delete") {
-//   if (action === "add") {
-//     arr.push(value);
-//   } else {
-//     const index = arr.indexOf(value);
-//     arr.splice(index, 1);
-//   }
-//   return arr;
-// }
-
-// workWithArr(fruit, "Banana", "add");
-// workWithArr(fruit, "Melon", "add");
-// workWithArr(fruit, "Pear", "add");
-// workWithArr(fruit, "Bear", "add");
-
-// console.log(fruit);
-
-// function calc(
-//   num1: number,
-//   num2: number,
-//   callback: (arg1: number, arg2: number) => number
-// ) {
-//   return callback(num1, num2);
-// }
-// function foo(num1: number, num2: number) {
+// function calc(num1: number, num2: number): number {
 //   return num1 + num2;
 // }
 
-// const result = calc(1, 3, foo);
-// console.log("result:", result);
+// function customError(): never {
+//   throw new Error("Error");
+// }
 
-// ПРАКТИКА
-// 1
-let age: number;
-// let name: string;
-let toggle: boolean;
-let empty: null;
-let notInitialize: undefined;
-let callback: (a: number) => number;
-callback = (a) => {
-  return 100 + a;
-};
+// type PageData = {
+//   title: string;
+//   likes: number;
+//   accounts: string[];
+//   status: string;
+//   details?: {
+//     createAt: Date;
+//     updateAt: Date;
+//   };
+// };
 
-// 2
-let anything: any;
-anything = "Text";
-anything = {};
+// const page1: PageData = {
+//   title: "The awesome page",
+//   likes: 100,
+//   accounts: ["Max", "Anton", "Nikita"],
+//   status: "open",
+//   details: {
+//     createAt: new Date("2021-01-01"),
+//     updateAt: new Date("2021-05-01"),
+//   },
+// };
 
-// 3
-let some: any;
-some = "Text";
-
-let str: string;
-str = some;
-
-// 4
-let person: [string, number];
-person = ["Max", 21];
-
-// 5
-enum Load {
-  LOADING,
-  READY,
-}
-
-const page = { load: Load.READY };
-
-if (page.load === Load.LOADING) {
-  console.log("Loading");
-}
-
-if (page.load === Load.READY) {
-  console.log("Ready");
-}
-
-// 6
-
-let strOrNumber: string | number;
-
-// 7
-let strNew: "enable" | "Disable";
-
-// 8
-function showMessage(message: string): void {
-  console.log(message);
-}
-
-function calc(num1: number, num2: number): number {
-  return num1 + num2;
-}
-
-function customError(): never {
-  throw new Error("Error");
-}
-
-type PageData = {
-  title: string;
-  likes: number;
-  accounts: string[];
-  status: string;
-  details?: {
-    createAt: Date;
-    updateAt: Date;
-  };
-};
-
-const page1: PageData = {
-  title: "The awesome page",
-  likes: 100,
-  accounts: ["Max", "Anton", "Nikita"],
-  status: "open",
-  details: {
-    createAt: new Date("2021-01-01"),
-    updateAt: new Date("2021-05-01"),
-  },
-};
-
-const page2: PageData = {
-  title: "Python or Js",
-  likes: 5,
-  accounts: ["Alex"],
-  status: "close",
-};
+// const page2: PageData = {
+//   title: "Python or Js",
+//   likes: 5,
+//   accounts: ["Alex"],
+//   status: "close",
+// };
--- src/module4.ts
+++ src/module4.ts
@@ -232,12 +232,6 @@
 
 // ======ПРАКТИКА=======
 
-abstract class House {
-  public door: "open" | "close";
-
-  public key=key;
-}
-
 class Key {
   private signature: number;
 
@@ -250,9 +244,50 @@ class Key {
   }
 }
 
-const key = new Key();
-
-class MyHouse extends House {
-    this.door = "open";
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
   }
 
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (!this.door) {
+      throw new Error("Door is close");
+    }
+    this.tenants.push(person);
+    console.log("Person inside");
+  }
+
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() !== this.key.getSignature()) {
+      throw new Error("Key to another door");
+    }
+
+    return (this.door = true);
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+console.log("house:", house);
+
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);

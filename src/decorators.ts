46e34bf5b309e3c83ef71c0c62741d5916f80db7
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-07-08 17:23:22 +0300

    decoration class

--- src/decorators.ts
+++ src/decorators.ts
@@ -20,27 +20,82 @@
 //   public id = 1;
 // }
 
-function Logger(logString: string) {
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
+// function Logger(logString: string) {
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+// function addProperty() {
+//   return function (constructor: Function) {
+//     console.log("Modify");
+//     constructor.prototype.modify = true;
+//   };
+// }
+
+// @Logger("LOGGING-CONTROLLER")
+// @addProperty()
+// class Controller {
+//   public id = 1;
+//   public modify?: boolean;
+// }
+
+// const controller = new Controller();
+
+// console.log("Modified classes", controller.modify);
+
+// function Logger(logString: string) {
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+// function AddProperty() {
+//   return function (constructor: Function) {
+//     console.log("Add property");
+//     constructor.prototype.modify = true;
+//   };
+// }
+
+// @Logger("LOGGING CONTROLLER")
+// @AddProperty()
+// class Controller {
+//   public ID = 1;
+//   public modify?: boolean;
+// }
+
+// const controller = new Controller();
+
+// console.log("Is modify?", controller.modify);
+
+interface IDecoration {
+  parent: string;
+  template: string;
+}
+
+function ControllerDecoration(config: IDecoration) {
+  return function <T extends { new (...arg: any[]): { content: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      private element: HTMLElement;
+      private parent: HTMLElement;
+      constructor(...args: any[]) {
+        super(...args);
+        this.parent = document.getElementById(config.parent)!;
+        this.element = document.createElement(config.template);
+
+        this.element.innerHTML = this.content;
+        this.parent.appendChild(this.element);
+      }
+    };
   };
 }
 
-function addProperty() {
-  return function (constructor: Function) {
-    console.log("Modify");
-    constructor.prototype.modify = true;
-  };
-}
-
-@Logger("LOGGING-CONTROLLER")
-@addProperty()
+@ControllerDecoration({ parent: "app", template: "H1" })
 class Controller {
-  public id = 1;
-  public modify?: boolean;
+  public content = "My controller";
 }
 
 const controller = new Controller();
-
-console.log("Modified classes", controller.modify);

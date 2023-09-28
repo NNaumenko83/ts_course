bc8b66e780a0ac116e7008ea0c16fd90c895f6d5
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-28 23:05:29 +0300

    done kata

--- src/module4.ts
+++ src/module4.ts
@@ -916,10 +916,31 @@
 
 // https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/typescript
 
-const towerBuilder = (nFloors: number): string[] => {
-  const arr = Array.from({ length: nFloors });
+// const towerBuilder = (nFloors: number): string[] => {
+//   const arr = Array.from({ length: nFloors });
 
-  return ["*"];
-};
+//   return ["*"];
+// };
 
-towerBuilder(1);
+// towerBuilder(1);
+
+// https://www.codewars.com/kata/5949481f86420f59480000e7/train/typescript
+
+function oddOrEven(array: number[]) {
+  if (
+    array.length === 0 ||
+    (array.length === 1 && array[array.length - 1] === 0)
+  ) {
+    return "even";
+  }
+
+  const value = array.reduce((acc, item) => acc + item, 0);
+
+  if (value % 2 === 0) {
+    return "even";
+  }
+  return "odd";
+}
+
+// oddOrEven([0]);
+console.log("oddOrEven([0]);:", oddOrEven([1, 2, 5, 7]));

8ade104b10737b5941555565f3d90f9bf509b7ba
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-10-29 22:18:06 +0200

    DONE: string to array

--- src/module4.ts
+++ src/module4.ts
@@ -1710,16 +1710,24 @@ function cookingTime(eggs: number): number {
 
 // https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/typescript
 
-function firstNonConsecutive(arr: number[]): null | number {
-  for (let i = 0; i < arr.length - 1; i += 1) {
-    if (arr[i + 1] - arr[i] === 1) {
-      continue;
-    }
+// function firstNonConsecutive(arr: number[]): null | number {
+//   for (let i = 0; i < arr.length - 1; i += 1) {
+//     if (arr[i + 1] - arr[i] === 1) {
+//       continue;
+//     }
 
-    return arr[i + 1];
-  }
+//     return arr[i + 1];
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
-firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]);
+// firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]);
+
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/typescript
+
+// function stringToArray(s: string): string[] {
+//   return s.split(" ");
+// }
+
+// stringToArray("Robin Singh");

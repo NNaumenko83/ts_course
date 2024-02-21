d6aa4adfbabf7e88052f543621e7aa9081f30db1
Mykola <nnv2006@ukr.net>
2024-02-21 14:44:36 +0200

    Grasshopper - Summation

--- src/module4.ts
+++ src/module4.ts
@@ -2584,3 +2584,21 @@
 // }
 
 // console.log(stringToNumber("1234567890"));
+
+
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/typescript
+
+
+const summation = (num: number) => { 
+    let res=0;
+    for (let i = 0; i <= num; i++) {
+
+       res+=i   
+     
+      
+    }
+
+    return res
+}
+
+console.log(summation(8))

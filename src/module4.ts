5ba941618380fe98940f5b518bbd2d52831a3156
Mykola <nnv2006@ukr.net>
2024-06-14 15:28:00 +0300

    Task 3 Generic

--- src/module4.ts
+++ src/module4.ts
@@ -3929,3 +3929,11 @@ function compare (top:Pick<AllType,"name"|"color">, bottom:Pick<AllType, "positi
     weight: bottom.weight,
   }
 }
+
+// Task 3
+// У вас є функція merge, яка поєднує два об'єкти.
+// Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
+
+function merge<T extends object, U extends object> (objA:T, objB:U) {
+  return Object.assign(objA, objB);
+}
